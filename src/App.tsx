975f737a7b252474e8b2297162b76d2ea512f418
fsilva <fsilva@urbano.com.pe>
2025-01-23 10:51:07 -0500

    cambios de pre-ruta de points

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import {  View } from 'react-native';
+ 
+import { HomeScreen } from './presentation/screens/home/HomeScreen';
+
+ 
+
+function App()  { 
+
+  return (
+    <View  > 
+      <HomeScreen/>
+    </View>
+  );
+}
+ 
+
+export default App;
